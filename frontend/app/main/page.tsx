692ca8fe34d243a55a6bf88b6cc47af9f2d154d7
Jaewon Moon <70671293+jaewonmoon00@users.noreply.github.com>
2024-01-21 07:09:12 -0500

    remove landmark from real time data

--- frontend/app/main/page.tsx
+++ frontend/app/main/page.tsx
@@ -1,16 +1,27 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 const Page = () => {
+  const [isCameraOn, setCameraOn] = useState(true);
   const searchParams = useSearchParams();
   const partySize = searchParams.get("partySize")?.toString();
+
+  const handleCameraError = () => {
+    setCameraOn(false);
+  };
   return (
     <div>
-      <img
-        src={`http://localhost:8000/video_feed/?partySize=${partySize}`}
-        alt="Webcam Feed"
-      />
+      {isCameraOn ? (
+        <img
+          src={`http://localhost:8000/video_feed/?partySize=${partySize}`}
+          alt="Webcam Feed"
+          onError={handleCameraError}
+        />
+      ) : (
+        console.log("Camera is off"),
+        <p>Camera is off</p>
+      )}
     </div>
   );
 };
